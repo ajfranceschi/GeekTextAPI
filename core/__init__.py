ded9ce5ff8a6e29981ea3db623939c14582678a3
Antonio J. Franceschi <afran244@fiu.edu>
2022-02-12 01:18:10 -0500

    added SQLAlchemy DB URI config to flask app in core/__init__.py; Updated gitignore to exclude binaries

--- core/__init__.py
+++ core/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from core.DB_CREDS import *
 
 # initialize Flask
 app = Flask(__name__)
+
+# Configure Database connection
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}'
