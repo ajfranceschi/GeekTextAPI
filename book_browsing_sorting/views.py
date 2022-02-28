89f30679f43c0057ff46283d55a5379c125dec6c
Antonio J. Franceschi <afran244@fiu.edu>
2022-02-27 21:58:09 -0500

    updated /get-books route, added top ten books sold route

--- book_browsing_sorting/views.py
+++ book_browsing_sorting/views.py
@@ -3,12 +3,25 @@ from . import bkBrowseSort_bp
 from models.db_book_model import Books, books_schema
 
 
+# Root Book Browsing and Sorting endpoint: (*/book-browsing-sorting/)
 @bkBrowseSort_bp.route('/')
 def index():
     return "Books Browsing and Sorting root"
 
 
-@bkBrowseSort_bp.route('/getbooks')
+# Book Browsing and Sorting / Get All Books endpoint: (*/book-browsing-sorting/get-books)
+@bkBrowseSort_bp.route('/get-books')
 def getBooks():
-    books = Books.query.all()
+    booksQuery = Books.query.all()
+    books = books_schema.dump(booksQuery)
+    for book in books:
+        print(book)
     return jsonify(books_schema.dump(books))
+
+
+# Book Browsing and Sorting / Get top 10 books sold endpoint: (*/book-browsing-sorting/top-ten-books-sold)
+@bkBrowseSort_bp.route('/top-ten-books-sold')
+def topTenBooksSold():
+    # TODO: Top ten books sold
+    #    Sort by unitsSold
+    return "Top Ten Books Sold route"
