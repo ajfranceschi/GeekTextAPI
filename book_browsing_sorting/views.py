3470da856117dd98890456a59855b065a5d076d1
Antonio J. Franceschi <afran244@fiu.edu>
2022-03-11 01:24:52 -0500

    updated book model to join data from Author database when queried.

--- book_browsing_sorting/views.py
+++ book_browsing_sorting/views.py
@@ -1,6 +1,7 @@
 from flask import jsonify
 from . import bkBrowseSort_bp
 from models.db_book_model import Books, books_schema
+from models.db_author_model import Authors, Author_schema, Authors_schema
 
 
 # Root Book Browsing and Sorting endpoint: (*/book-browsing-sorting/)
@@ -13,15 +14,17 @@ def index():
 @bkBrowseSort_bp.route('/get-books')
 def getBooks():
     booksQuery = Books.query.all()
-    books = books_schema.dump(booksQuery)
-    for book in books:
+    for book in booksQuery:
         print(book)
-    return jsonify(books_schema.dump(books))
+    booksList = books_schema.dump(booksQuery)
+    return jsonify(booksList)
 
 
 # Book Browsing and Sorting / Get top 10 books sold endpoint: (*/book-browsing-sorting/top-ten-books-sold)
 @bkBrowseSort_bp.route('/top-ten-books-sold')
 def topTenBooksSold():
     # TODO: Top ten books sold
-    #    Sort by unitsSold
+    #    Sort by unitsSold decreasing
     return "Top Ten Books Sold route"
+
+# UTILITY FUNCTIONS
--- models/db_book_model.py
+++ models/db_book_model.py
@@ -1,4 +1,6 @@
 from core import db, marshmallow
+from marshmallow import fields
+from . import db_author_model
 
 
 # Book model
@@ -14,15 +16,40 @@ class Books(db.Model):
     bookYearPublished = db.Column(db.Integer, nullable=False)
     unitsSold = db.Column(db.Integer)
     bookRating = db.Column(db.Float)
+    author = db.relationship('Authors', backref=db.backref('Authors', lazy='dynamic'))
+
+    def __init__(self, isbn, idAuthors, bookTitle, bookDescription, bookPrice, bookGenre, bookPublisher,
+                 bookYearPublished, unitsSold, bookRating):
+        self.isbn = isbn
+        self.idAuthors = idAuthors
+        self.bookTitle = bookTitle
+        self.bookDescription = bookDescription
+        self.bookPrice = bookPrice
+        self.bookGenre = bookGenre
+        self.bookPublisher = bookPublisher
+        self.bookYearPublished = bookYearPublished
+        self.unitsSold = unitsSold
+        self.bookRating = bookRating
 
 
 class BooksSchema(marshmallow.Schema):
+    isbn = fields.Int()
+    idAuthors = fields.Int()
+    bookTitle = fields.Str()
+    bookDescription = fields.Str()
+    bookPrice = fields.Number()
+    bookGenre = fields.Str()
+    bookPublisher = fields.Str()
+    bookYearPublished = fields.Number()
+    unitsSold = fields.Number()
+    bookRating = fields.Number()
+    author = fields.Nested(db_author_model.AuthorSchema)
+
     class Meta:
-        fields = ('isbn', 'idAuthors', 'bookTitle', 'bookDescription',
+        fields = ('isbn', 'idAuthor', 'bookTitle', 'bookDescription',
                   'bookPrice', 'bookGenre', 'bookPublisher', 'bookYearPublished',
-                  'unitsSold', 'bookRating')
+                  'unitsSold', 'bookRating', 'author')
 
 
 book_schema = BooksSchema()
 books_schema = BooksSchema(many=True)
-
