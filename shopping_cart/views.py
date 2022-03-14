b87f8a154e566b4d7ae8a0e06105f5664c35d235
NicoleNG72 <ngent004@fiu.edu>
2022-03-13 22:33:00 -0400

    new change upgrade

--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -1,6 +1,7 @@
-#from flask import jsonify
+from flask import jsonify,request
 from . import cart_bp
-#from models.db_Shopping_Carts import ShoppingCarts, ShoppingCarts_many_schema
+from models.db_Shopping_Carts import ShoppingCart, ShoppingCart_many_schema
+from models.db_Shopping_Cart_Items import ShoppingCartItems, ShoppingCartItems_many_schema
 
 
 @cart_bp.route('/')
@@ -8,6 +9,9 @@ def index():
     return "Shopping Cart route"
 
 
-@cart_bp.route('/getCart')
-def getCart():
-    return {"Route": "Get Shopping Cart"}
+@cart_bp.route('/get_cart_items', methods=['GET'])
+def get_cart_items():
+    cart_items_query = ShoppingCartItems.query.all()
+    cart_items_list = ShoppingCartItems_many_schema(cart_items_query)
+    return jsonify(cart_items_list.data)
+
