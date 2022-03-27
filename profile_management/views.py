84e7324e55749a07de6674e6173ebab637c59b75
Kevin Forero <kfore010@fiu.edu>
2022-03-26 23:51:23 -0400

    Update on Profile Management
    Added CC model and functions to add new user and search account by user

--- profile_management/views.py
+++ profile_management/views.py
@@ -1,4 +1,7 @@
+from flask import jsonify, request, Response
 from . import profman_bp
+from models.db_user_model import Users
+
 
 
 @profman_bp.route('/')
@@ -9,3 +12,32 @@ def index():
 @profman_bp.route('/getProf')
 def getProfile():
     return {"Route": "Get Profile Management"}
+
+# @profman_bp.route('/getAll')
+# def getUsers():
+#     userQuery = Users.query.all()
+#     allUsers = Users_schema.dump(userQuery)
+#     return jsonify(allUsers), 200
+
+@profman_bp.route('/getUserInfo/<string:username>/', methods=['GET'])
+def getUserInfo(username):
+    returnUser = Users.infoByUser(username)
+    return jsonify(returnUser)
+
+@profman_bp.route('/addUser', methods=['POST'])
+def addUser():
+    first_name = request.args.get('first_name')
+    last_name = request.args.get('last_name')
+    username = request.args.get('username')
+    passwordU = request.args.get('passwordU')
+    emailAddress = request.args.get('emailAddress')
+    addressLine1 = request.args.get('addressLine1')
+    addressLine2 = request.args.get('addressLine2')
+    city = request.args.get('city')
+    state = request.args.get('state')
+    zipcode = request.args.get('zipcode')
+
+
+    Users.createUser(first_name, last_name, username, passwordU, emailAddress, addressLine1, addressLine2, city, state, zipcode)
+    response = Response("New User added!", 200, mimetype='application/json')
+    return response
