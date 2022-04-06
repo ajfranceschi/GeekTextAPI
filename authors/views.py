b544ee0da7e926ba8dbd09b3f888698bf8d5ef3e
Carlos Gonzalez <cmdelapaz2014@gmail.com>
2022-04-06 00:45:30 -0400

    Validation

--- authors/views.py
+++ authors/views.py
@@ -1,3 +1,9 @@
+# ***************************************************************
+# Author by: Carlos Gonzalez
+#
+# This file contains the routes for GeekText API
+# Authors
+# ****************************************************************
 from flask import jsonify, request, Response, abort
 from . import author_bp
 from models.db_author_model import Authors, Authors_schema
@@ -9,10 +15,14 @@ def index():
 
 @author_bp.route('/addAuthor', methods=['POST'])
 def addAuthor():
-    authorFirstName = request.args.get('authorFirstName')
-    authorLastName = request.args.get('authorLastName')
-    authorPublisher = request.args.get('authorPublisher')
-    authorBiography = request.args.get('authorBiography')
-    Authors.createAuthor(authorFirstName, authorLastName, authorPublisher, authorBiography)
-    response = Response("New author added!", 200, mimetype='application/json')
-    return response
+    try:
+        authorFirstName = str(request.args.get('authorFirstName'))
+        authorLastName = str(request.args.get('authorLastName'))
+        authorPublisher = str(request.args.get('authorPublisher'))
+        authorBiography = str(request.args.get('authorBiography'))
+        Authors.createAuthor(authorFirstName, authorLastName, authorPublisher, authorBiography)
+        response = Response("New author added!", 200, mimetype='application/json')
+        return response
+    except ValueError as e:
+        print(e)
+        return 'Params are not accurate.', 400
